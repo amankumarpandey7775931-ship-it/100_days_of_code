b5f3e7076fcc9c3589a6e597eb2637d6a0f2937a
Aman pandey <amankumarpandey7775931@gmail.com>
2025-11-05 01:51:31 +0530

    Add profit and loss percentage calculation program
    This program calculates profit or loss percentage based on user input for cost price and selling price.

---
+++ Day:11--20/Q22.c
@@ -0,0 +1,31 @@
+/*Q22:Write a program to find profit or loss percentage given cost price and selling price
+*/
+#include <stdio.h>
+
+int main() {
+    float costPrice, sellingPrice, profitOrLoss, percentage;
+
+    printf("Enter Cost Price: ");
+    scanf("%f", &costPrice);
+
+    printf("Enter Selling Price: ");
+    scanf("%f", &sellingPrice);
+
+    if (sellingPrice > costPrice) {
+        profitOrLoss = sellingPrice - costPrice;
+        percentage = (profitOrLoss / costPrice) * 100;
+        printf("You made a Profit of %.2f\n", profitOrLoss);
+        printf("Profit Percentage = %.2f%%\n", percentage);
+    } 
+    else if (sellingPrice < costPrice) {
+        profitOrLoss = costPrice - sellingPrice;
+        percentage = (profitOrLoss / costPrice) * 100;
+        printf("You incurred a Loss of %.2f\n", profitOrLoss);
+        printf("Loss Percentage = %.2f%%\n", percentage);
+    } 
+    else {
+        printf("No Profit, No Loss.\n");
+    }
+
+    return 0;
+}
